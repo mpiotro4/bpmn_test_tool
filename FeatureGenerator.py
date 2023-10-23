bd7132f4862c8472c6e8c9abd4cb4eba1bc5e492
mpio <mpiotro4@outlook.com>
2023-10-23 16:53:28 +0200

    private functions

--- FeatureGenerator.py
+++ FeatureGenerator.py
@@ -3,18 +3,18 @@ class FeatureGenerator:
     def generate_feature(filename, all_paths):
         with open(filename, "w") as file:
             for index, path in enumerate(all_paths):
-                FeatureGenerator.generate_test_case(file, path, index)
+                FeatureGenerator._generate_test_case(file, path, index)
 
     @staticmethod
-    def generate_test_case(file, path, index):
+    def _generate_test_case(file, path, index):
         file.write(f"Scenario: scenario {index}\n")
         for index, node in enumerate(path):
-            FeatureGenerator.generate_prefix(index, file)
-            FeatureGenerator.generate_step(node, file)
+            FeatureGenerator._generate_prefix(index, file)
+            FeatureGenerator._generate_step(node, file)
         file.write("\n")
 
     @staticmethod
-    def generate_step(node, file):
+    def _generate_step(node, file):
         file.write(node.name)
         if node.decision_var is not None:
             if node.decision_var_val is not None:
@@ -24,7 +24,7 @@ class FeatureGenerator:
         file.write("\n")
 
     @staticmethod
-    def generate_prefix(index, file):
+    def _generate_prefix(index, file):
         if index == 0:
             file.write("Given ")
         if (index + 1) % 2 == 0:
