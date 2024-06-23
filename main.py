b9d0f1aae2fba093f436e1c0c63e3d3c7c0cabdb
mpio <mpiotro4@outlook.com>
2024-06-23 22:33:02 +0200

    put all input data into main function

--- main.py
+++ main.py
@@ -1,31 +1,54 @@
 from GraphCompatibilityAnalyzer.GraphCompatibilityAnalyzer import generate_reports
 from demo import *
+from xmi.UseCaseWrapper import UseCaseWrapper
 
 if __name__ == "__main__":
-    xmi_graph = XmiWrapper('source_data/bankomat_2.xml')
-    graphs = xmi_graph.get_graphs()
-    graph = xmi_graph.use_cases_wrappers[1].scenario_wrapper.get_graph()
-    node_labels = {node: node.name for node in graph.nodes()}
-    # GraphVisualizer.visualize_graph(graph, node_labels)
-
-    # for graph in graphs:
-    #     node_labels = {node: node.name for node in graph.nodes()}
-    #     GraphVisualizer.visualize_graph(graph, node_labels)
-
     bpmnWrappers = []
-    bpmnFilesPaths = ["source_data/bpmn/autoryzacja_uzytkownika.bpmn", "source_data/bpmn/Wplata.bpmn"]
+    bpmnFilesPaths = ["source_data/bpmn/autoryzacja_uzytkownika.bpmn",
+                      "source_data/bpmn/Obsluga_sprawdzenia_salda.bpmn",
+                      "source_data/bpmn/Obsluga_wplaty.bpmn",
+                      "source_data/bpmn/Obsluga_wyplaty.bpmn",
+                      "source_data/bpmn/sprawdzenie_pin.bpmn",
+                      "source_data/bpmn/Sprawdzenie_salda.bpmn",
+                      "source_data/bpmn/Wplata.bpmn",
+                      "source_data/bpmn/Wyplata.bpmn"
+                      ]
+
     for bpmnFilePath in bpmnFilesPaths:
         bpmnWrappers.append(BpmnWrapper(bpmnFilePath))
 
-    bpmn_wrapper_1 = bpmnWrappers[0]
-    bpmn_wrapper_2 = bpmnWrappers[1]
-    node_labels = {node: node.name for node in bpmn_wrapper_2.get_graph().nodes()}
-    # GraphVisualizer.visualize_graph(bpmn_graph_2.get_graph(), node_labels)
+    bpmn_autoryzacja = bpmnWrappers[0]
+    bpmn_obsluga_sprawdzenia_salda = bpmnWrappers[1]
+    bpmn_obsluga_wplaty = bpmnWrappers[2]
+    bpmn_obsluga_wyplaty = bpmnWrappers[3]
+    bpmn_sprawdzenie_pin = bpmnWrappers[4]
+    bpmn_sprawdzenie_salda = bpmnWrappers[5]
+    bpmn_wplata = bpmnWrappers[6]
+    bpmn_wyplata = bpmnWrappers[7]
 
-    paths_a = xmi_graph.use_cases_wrappers[0].scenario_wrapper.get_paths()
-    paths_b = bpmn_wrapper_1.get_paths()
+    xmi_wrapper = XmiWrapper('source_data/bankomat_2.xml')
+    use_case_autoryzacja = xmi_wrapper.use_cases_wrappers[0]
+    use_case_obsluga_sprawdzenia_salda = xmi_wrapper.use_cases_wrappers[1]
+    use_case_obsluga_wplaty = xmi_wrapper.use_cases_wrappers[2]
+    use_case_obsluga_wyplaty = xmi_wrapper.use_cases_wrappers[3]
+    use_case_sprawdznie_pin = xmi_wrapper.use_cases_wrappers[4]
+    use_case_sprawdzenie_salda = xmi_wrapper.use_cases_wrappers[5]
+    use_case_wplata = xmi_wrapper.use_cases_wrappers[6]
+    use_case_wyplata = xmi_wrapper.use_cases_wrappers[7]
 
-    tuple1 = (xmi_graph.use_cases_wrappers[0], bpmnWrappers[0])
-    tuple2 = (xmi_graph.use_cases_wrappers[1], bpmnWrappers[1])
-    tuple_list = [tuple1, tuple2]
-    generate_reports(tuple_list)
+    graph = use_case_wyplata.scenario_wrapper.get_graph()
+    # node_labels = {node: node.name for node in graph.nodes()}
+    # GraphVisualizer.visualize_graph(graph, node_labels)
+
+    graph_wrappers = [
+        (use_case_autoryzacja, bpmn_autoryzacja),
+        (use_case_obsluga_sprawdzenia_salda, bpmn_obsluga_sprawdzenia_salda),
+        (use_case_obsluga_wyplaty, bpmn_obsluga_wyplaty),
+        (use_case_obsluga_wplaty, bpmn_obsluga_wplaty),
+        (use_case_sprawdznie_pin, bpmn_sprawdzenie_pin),
+        (use_case_sprawdzenie_salda, bpmn_sprawdzenie_salda),
+        (use_case_wplata, bpmn_wplata),
+        (use_case_wyplata, bpmn_wyplata)
+    ]
+
+    generate_reports(graph_wrappers)
